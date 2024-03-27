5e934e15762a62cbcf98b2799963e23b878519be
noeh1001 <noeh1001@stud.hs-kl.de>
2024-03-27 16:01:26 +0100

    map initialisiert

--- app/src/main/java/com/example/citycare/LandingPage.java
+++ app/src/main/java/com/example/citycare/LandingPage.java
@@ -1,14 +1,43 @@
 package com.example.citycare;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LandingPage extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+public class LandingPage extends AppCompatActivity implements MapListener {
+
+    MapView mMap;
+    Marker poiMarker;
+    IMapController controller;
+    MyLocationNewOverlay mMyLocationOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +49,115 @@ public class LandingPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initPermissions();
+    }
+
+    @SuppressLint("ObsoleteSdkInt")
+    protected void initPermissions(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            loadMap();
+            updateLocation();
+        }else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            loadMap();
+            updateLocation();
+        }
+    }
+
+    protected void loadMap(){
+        Configuration.getInstance().load(
+                getApplicationContext(),
+                getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+        );
+
+        mMap = findViewById(R.id.osmmap);
+        mMap.setTileSource(TileSourceFactory.MAPNIK);
+        mMap.setMultiTouchControls(true);
+        mMap.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
+
+        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint geoPoint) {
+                updatePoiMarker(new GeoPoint(geoPoint.getLatitude(), geoPoint.getLongitude()));
+                return true;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint geoPoint) {
+                return false;
+            }
+        });
+
+
+        mMyLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mMap);
+        controller = mMap.getController();
+
+        mMyLocationOverlay.enableMyLocation();
+        mMyLocationOverlay.enableFollowLocation();
+        mMyLocationOverlay.setDrawAccuracyEnabled(true);
+        mMyLocationOverlay.runOnFirstFix(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        controller.setCenter(mMyLocationOverlay.getMyLocation());
+                        controller.animateTo(mMyLocationOverlay.getMyLocation());
+                    }
+                });
+            }
+        });
+        controller.setZoom(18.0);
+        mMap.getOverlays().add(0, mapEventsOverlay); // Das Overlay an den Anfang der Liste hinzufügen
+        mMap.getOverlays().add(mMyLocationOverlay);
+        mMap.invalidate();
+        mMap.addMapListener(this);
+    }
+
+    private void updatePoiMarker(GeoPoint geoPoint) {
+        if(poiMarker != null){
+            mMap.getOverlays().remove(poiMarker);
+        }
+
+        poiMarker = new Marker(mMap);
+        poiMarker.setPosition(geoPoint);
+        //poiMarker.setIcon(ContextCompat.getDrawable(this, R.mipmap.poiklein));
+        mMap.getOverlays().add(poiMarker);
+
+    }
+
+    @SuppressLint("MissingPermission")
+    protected void updateLocation(){
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this::onLocationReceived);
+    }
+
+    protected void onLocationReceived(Location location){
+
+    }
+
+    @Override
+    public boolean onScroll(ScrollEvent event) {
+        return true;
+    }
+
+    @Override
+    public boolean onZoom(ZoomEvent event) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
