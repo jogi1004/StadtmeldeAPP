75d8e788881099f1f9905c33d432b3fa1d42561e
4ndr34sss <andre.bieber06@gmail.com>
2024-04-03 13:44:37 +0200

    HelperAPI erweitert

--- app/src/main/java/com/example/citycare/util/APIHelper.java
+++ app/src/main/java/com/example/citycare/util/APIHelper.java
@@ -1,6 +1,23 @@
 package com.example.citycare.util;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class APIHelper {
     private String postURL = "https://backendservice-dev-5rt6jcn4da-uc.a.run.app/auth/register";
+
+    public void registerUser(String username, String email, String password){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, postURL, null, new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+
+
+                    }
+                }
+    }
 
 }
