e5102325864db4dbdc1983da4c5304eb4a9f0fbf
4ndr34sss <andre.bieber06@gmail.com>
2024-04-15 10:51:52 +0200

    DetilView Prototyp wird aufgerufen und mit Titel ausgestattet, RecyclerView jetzt mit korrekten Titeln

--- app/src/main/java/com/example/citycare/Dialogs/DialogReportDetailView.java
+++ app/src/main/java/com/example/citycare/Dialogs/DialogReportDetailView.java
@@ -2,12 +2,41 @@ package com.example.citycare.Dialogs;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Base64;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
-public class DialogReportDetailView extends Dialog {
-    public DialogReportDetailView(@NonNull Context context) {
-        super(context);
-    }
+import com.example.citycare.R;
+import com.example.citycare.model.SubCategoryModel;
 
+public class DialogReportDetailView extends Dialog {
+    TextView headline,descriptionReport,coordinates;
+    ImageView imageReport;
+    Bitmap imageBitmap;
+
+    public DialogReportDetailView(@NonNull Context context,String subCategory, String image, String description, double longitude, double latitude, String timestamp) {
+        super(context);
+        setContentView(R.layout.dialog_report_detail_view);
+        headline = findViewById(R.id.headlineReport);
+        imageReport = findViewById(R.id.reportImage);
+        descriptionReport = findViewById(R.id.descriptionReportDetailView);
+        coordinates = findViewById(R.id.coordinates);
+
+        headline.setText(subCategory);
+        if(image != null){
+            byte[] decodedBytes = Base64.decode(image, Base64.DEFAULT);
+            imageBitmap = BitmapFactory.decodeByteArray(decodedBytes,0, decodedBytes.length);
+            imageReport.setImageBitmap(imageBitmap);
+        }
+        descriptionReport.setText(description);
+        String coordinatesReport = longitude + ", " + latitude;
+        coordinates.setText(coordinatesReport);
+
+    }
 }
--- app/src/main/java/com/example/citycare/Dialogs/ReportDialogPage.java
+++ app/src/main/java/com/example/citycare/Dialogs/ReportDialogPage.java
@@ -1,28 +1,26 @@
 package com.example.citycare.Dialogs;
 
+
 import android.app.Dialog;
 import android.content.Context;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.Toast;
-
 import com.example.citycare.LandingPage;
 import com.example.citycare.R;
 import com.example.citycare.model.ReportModel;
-import com.example.citycare.adapter.RecyclerViewAdapter_AllReports;
-import com.example.citycare.util.APIHelper;
+import com.example.citycare.adapter.RecyclerViewAdapter_AllReports;;
 import com.example.citycare.util.RecyclerViewInterface;
-
 import java.util.ArrayList;
 
 public class ReportDialogPage extends Dialog implements RecyclerViewInterface {
@@ -30,10 +28,14 @@ public class ReportDialogPage extends Dialog implements RecyclerViewInterface {
     Context context;
     FrameLayout dim;
     ArrayList<ReportModel> allReports = new ArrayList<>();
+    private int dialogheight;
     public ReportDialogPage(@NonNull Context context) {
         super(context);
         this.context = context;
         this.dim = findViewById(R.id.dimm);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        dialogheight = (int) (displayMetrics.heightPixels*0.7);
+
     }
 
     @Override
@@ -57,6 +59,9 @@ public class ReportDialogPage extends Dialog implements RecyclerViewInterface {
     @Override
     public void onItemClick(int position) {
         Toast.makeText(context, "Item " + position + " angeklickt", Toast.LENGTH_SHORT).show();
-        Intent Intent = new Intent(context,DialogReportDetailView.class);
+        ReportModel ClickedReport = allReports.get(position);
+        DialogReportDetailView DetailView = new DialogReportDetailView(context,ClickedReport.getTitle(),ClickedReport.getImage(),ClickedReport.getDescription(),ClickedReport.getLatitude(),ClickedReport.getLongitude(),ClickedReport.getTimestamp());
+        DetailView.show();
+        Log.d("DetailView", "Dialog wird jetzt angezeigt...");
     }
 }
