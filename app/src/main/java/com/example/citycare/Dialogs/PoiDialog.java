54aa52474266d72cb5a8b80a432a65e32263d5d8
Ibot96 <tobias.schuck1996@gmail.com>
2024-04-26 12:33:08 +0200

    poi on click implementiert für kurzinfos zur meldung

---
+++ app/src/main/java/com/example/citycare/Dialogs/PoiDialog.java
@@ -0,0 +1,82 @@
+package com.example.citycare.Dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.citycare.R;
+import com.example.citycare.model.ReportModel;
+
+import java.io.IOException;
+import java.util.List;
+
+public class PoiDialog extends Dialog {
+
+    private ImageView imageView;
+    private TextView title, description, adress;
+    private Context context;
+    private int dialogheight;
+    private ReportModel m;
+    public PoiDialog(@NonNull Context context, ReportModel m) {
+        super(context);
+        this.context = context;
+        this.m = m;
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_poi_onclick);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        imageView =  findViewById(R.id.poi_report_pic);
+        title = findViewById(R.id.poiTitle);
+        description = findViewById(R.id.poiDescription);
+        adress = findViewById(R.id.poiAdress);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        if (windowManager != null) {
+            windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        }
+        dialogheight = (int) (displayMetrics.heightPixels * 0.4);
+
+        getWindow().setGravity(Gravity.BOTTOM);
+        getWindow().setLayout(FrameLayout.LayoutParams.MATCH_PARENT, dialogheight);
+        getWindow().setDimAmount(0.0f);
+
+
+        String cityname;
+        if (m.getImage()!=null){
+            imageView.setImageBitmap(m.getImage());
+        }
+        title.setText(m.getTitle());
+        if (m.getDescription() != null){
+            description.setText(m.getDescription());
+        }else {
+            description.setText("keine Beschreibung");
+        }
+        Geocoder geocoder = new Geocoder(context);
+        try {
+            List<Address> addresses = geocoder.getFromLocation(m.getLatitude(), m.getLongitude(), 1);
+            cityname = addresses.get(0).getLocality();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        adress.setText(cityname);
+    }
+}
